3d68322429dda5d1f1b5b42c557bce422657706d
Libero Suprani <lsuprani30@gmail.com>
2024-11-25 17:07:36 +0000

    falta implementar melhor a validacao das cores pelo usuario no Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,6 +16,39 @@ import types.MultiColourMastermindGame;
 public class Main {
 	
 	final static int codeSize = 4;
+	final static int OG_MASTERMIND_OPTION = 1;
+	final static int BULLS_AND_COWS_OPTION = 2;
+	
+	public static String validLetters(int option) {
+		
+		StringBuilder result = new StringBuilder("[");
+		
+		if (option == OG_MASTERMIND_OPTION) 
+			for (MultiColour m : MultiColour.values()) 
+				result.append(m.toString() + ", ");	
+		
+		else 
+			for (BinaryColour m : BinaryColour.values()) 
+				result.append(m.toString() + ", ");	
+		
+		result.delete(result.length()-2, result.length());
+		result.append("]");
+		
+		return result.toString();
+	}
+	
+	public static boolean isValidColourFromUserTrial(char colour, int gameOption) {
+		if (gameOption == OG_MASTERMIND_OPTION) {
+			if (MultiColour.fromChar(Character.toUpperCase(colour)) == null)
+				return false;
+			return true;
+		}
+		else {
+			if (BinaryColour.fromChar(Character.toUpperCase(colour)) == null)
+				return false;
+			return true;
+		}
+	}
 	
     public static void main(String[] args) {
 
@@ -25,12 +58,12 @@ public class Main {
         int option = 0;
         System.out.println("Welcome to the Mastermind game!\n\nTo play, please choose an option.\n");
 
-        while (option != 1 && option != 2 && option != 3) {
+        while (option != OG_MASTERMIND_OPTION && option != BULLS_AND_COWS_OPTION && option != 3) {
 	        System.out.print("1-Original Mastermind\n2-Bulls and cows\n3-Exit\nOption: ");
 	        option = sc.nextInt();
 	        sc.nextLine();
 	        
-	        if (option != 1 && option != 2 && option != 3) {
+	        if (option != OG_MASTERMIND_OPTION && option != BULLS_AND_COWS_OPTION && option != 3) {
 	        	System.out.printf("\n--------------------\nInvalid option!\n\n");
 	        }
         }
@@ -41,7 +74,7 @@ public class Main {
         	Colour[] colours = null;
 
         	switch (option) {
-	        	case 1:
+	        	case OG_MASTERMIND_OPTION:
 	        		colours = MultiColour.values();
 	        		game = new MultiColourMastermindGame(random.nextInt(), codeSize, colours);
 	        		break;
@@ -59,52 +92,56 @@ public class Main {
 		        	System.out.println("Resposta: " + game.code.getCode()); //TODO remover isso aqui, só adicionei pra fazer debug
 		        	System.out.println(game.toString());
 		        	
+		        	
 		        	System.out.print("Attempt (ABCD) or Hint(.): ");
 		        	userTrialInput = sc.nextLine();
 		        	
-		        	while (userTrialInput.length() != codeSize && userTrialInput.charAt(0) != '.') {
-		        		System.out.println("Your attempt must have " + codeSize + " letters!");
-		        		System.out.print("Attempt (ABCD) or Hint(.): ");
-		        		userTrialInput = sc.nextLine();
-		        	}
-		        	
-		        	if (userTrialInput.charAt(0) == '.') {
+		        	if (userTrialInput.charAt(0) == '.') 
 		        		System.out.println("Hint: " + game.hint()); 
-		        		
-		        	}
+		        
 		        	else {
 			        	List<Colour> c = new ArrayList<Colour>();
 			        	for (int i = 0; i < codeSize; i++) {
-			        		if (option == 1) {
-			        			//TODO tratar exceção de um char não existir nesses enums 
-			        			c.add(MultiColour.fromChar(Character.toUpperCase(userTrialInput.charAt(i))));
-			        		}
-		        			else {
-		        				c.add(BinaryColour.fromChar(Character.toUpperCase(userTrialInput.charAt(i))));
-			        		}
+			        		Colour currentTrialColour = null;
 			        		
-			        	}
+			        		//TODO tem que acertar isso aqui
+			        		while (userTrialInput.length() != codeSize && isValidColourFromUserTrial(userTrialInput.charAt(i), option)) {
+				        		if (userTrialInput.length() != codeSize)
+				        			System.out.println("Your attempt must have " + codeSize + " letters!");
+				        		if (isValidColourFromUserTrial(userTrialInput.charAt(i), option))
+				        			System.out.println("Your attempt must have valid letters " + validLetters(option));
+				        		System.out.print("Attempt (ABCD) or Hint(.): ");
+				        		userTrialInput = sc.nextLine();
+				        	}
+			        		
+		        			if (option == OG_MASTERMIND_OPTION) 
+		        				currentTrialColour = MultiColour.fromChar(Character.toUpperCase(userTrialInput.charAt(i)));	        					
+		        			else  
+		        				currentTrialColour = BinaryColour.fromChar(Character.toUpperCase(userTrialInput.charAt(i)));	
+		        			
+		        			c.add(currentTrialColour);		
+		          		}
 			        	
 			        	Code userTrialCode = null;
-			        	if (option == 1) {
+			        	if (option == OG_MASTERMIND_OPTION) 
 			        		userTrialCode = new Code(c);
-		        		}
-		    			else {
-		    				// userTrialCode = new BullsAndCowsCode();
-		    				//TODO ainda falta implementar a classe BullsAndCowsCode
-		        		}
+			        	
+			        	else {
+			        		// userTrialCode = new BullsAndCowsCode();
+			        		//TODO ainda falta implementar a classe BullsAndCowsCode
+			        	}
 			        	
 			        	game.play(userTrialCode);
-		        	}
-		        }
+		        	}	
+		        
+	        	}
 	        	System.out.println("\nCongratulations, you cracked the code!\nWould you like to play again? (y/n) ");
 	        	anotherRound = sc.next().charAt(0);
 	        	sc.nextLine();
 	        	game.startNewRound();
-        	}
-        	while (anotherRound == 'y');
-	        
+	        	
+        	}while (anotherRound == 'y');
+    	
         }
     }
-
 }
